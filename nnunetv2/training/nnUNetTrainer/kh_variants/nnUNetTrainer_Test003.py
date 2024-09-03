30a4c0443a91189540b25e9a8c5c761773d0275c
K. Harrison <kh296@users.noreply.github.com>
2024-09-03 11:59:35 +0100

    Added configuration options

---
+++ nnunetv2/training/nnUNetTrainer/kh_variants/nnUNetTrainer_Test003.py
@@ -0,0 +1,20 @@
+import torch
+
+from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
+
+
+class nnUNetTrainer_Test003(nnUNetTrainer):
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+                 device: torch.device = torch.device('cuda')):
+        """used for debugging plans etc"""
+        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        self.num_iterations_per_epoch = 150
+        self.num_val_iterations_per_epoch = 30
+        self.num_epochs = 100
+
+    def configure_rotation_dummyDA_mirroring_and_inital_patch_size(self):
+        rotation_for_DA, do_dummy_2d_data_aug, initial_patch_size, mirror_axes = \
+            super().configure_rotation_dummyDA_mirroring_and_inital_patch_size()
+        mirror_axes = None
+        self.inference_allowed_mirroring_axes = None
+        return rotation_for_DA, do_dummy_2d_data_aug, initial_patch_size, mirror_axes
